87f22dbc8ece1b3d32277f6c63a2c84b1222ae0b
graham <conzett@gmail.com>
2011-05-01 13:27:19 -0400

    Add FPS counter

--- tetris.js
+++ tetris.js
@@ -87,6 +87,7 @@ var game = function(canvas, level, score, time){
 	this.score = score || 0;
 	this.time = time || 100;
 	this.status = "play";
+	this.fps = 0;
 	var that = this;
 	
 	window.addEventListener('keydown', function(event) {
@@ -108,16 +109,24 @@ var game = function(canvas, level, score, time){
 		
 	}, false );
 	
+	this.fpsLoop = function(){
+		document.getElementById('fps').innerHTML = that.fps;
+		that.fps = 0;
+		setTimeout ( that.fpsLoop, 1000);
+	}
+	
 	this.renderLoop = function(){
 	
 		context.clearRect(0, 0, that.level.structure[0].length, that.level.structure.length);
-		if( that.status !== "stop" ){			
-			setTimeout ( that.renderLoop, 100);
+		if( that.status !== "stop" ){
+			that.fps ++;
+			setTimeout ( that.renderLoop, 10);
 		}
 	}
 	
 	if (this.canvas.getContext) {
 		var context = this.canvas.getContext('2d');
+		this.fpsLoop();
 		this.renderLoop();
 	} else {
 		alert("Canvas Not Supported");
