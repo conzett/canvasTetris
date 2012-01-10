30b222c5f80717e96fae18f888b40a3abd4ecc04
Graham Conzett <conzett@gmail.com>
2012-01-09 20:44:05 -0500

    Make members of piece function private with getters and setters
    Add several get, set and other accessor methods to the piece clojure that alter and get it's private member variables. Refactor the rest of the program to use these new functions.

--- tetris.js
+++ tetris.js
@@ -1,29 +1,67 @@
 var piece = function(structure, color, top, left){	
 	
-	this.structure = structure || [[1]];
-	this.top = (-1 * structure.length)
-	this.left = (left - structure[0].length +1);
-	this.height = structure.length;
-	this.width = structure[0].length;
-	this.color = color || '#000000';
+	var structure = structure || [[1]];
+	var top = (-1 * structure.length)
+	var left = (left - structure[0].length +1);
+	var height = structure.length;
+	var width = structure[0].length;
+	var color = color || '#000000';
+
+	this.getStructure = function(){
+		return structure.slice(0); // Return copy
+	}
+
+	this.getTop = function() {
+		return top;
+	}
+
+	this.getLeft = function() {
+		return left;
+	}
+
+	this.getWidth = function() {
+		return structure[0].length;
+	}
+
+	this.getHeight = function() {
+		return structure.length;
+	}
+
+	this.getColor = function() {
+		return color;
+	}
+
+	this.drop = function() {
+		top = top + 1;
+	}
+
+	this.moveLeft = function() {
+		left = left - 1;
+	}
+
+	this.moveRight = function() {
+		left = left + 1;
+	}
+
+	this.setLeft = function(increment) {
+		left = increment;
+	}
 	
 	this.rotate = function(direction){
 		
-		var temp = new Array(this.structure[0].length);
+		var temp = new Array(structure[0].length);
 		var i, j;
 		for(i = 0; i < temp.length; ++i){
-			temp[i] = new Array(this.structure.length);
+			temp[i] = new Array(structure.length);
 			for (j = 0; j < temp[0].length; ++j){
 				if(direction === 'ccw'){
-					temp[i][j] = this.structure[j][temp.length - i - 1];
+					temp[i][j] = structure[j][temp.length - i - 1];
 				}else if(direction === 'cw'){
-					temp[i][j] = this.structure[temp[0].length - j - 1][i];
+					temp[i][j] = structure[temp[0].length - j - 1][i];
 				}				
 			}
 		}
-		this.structure = temp;
-		this.height = temp.length;
-		this.width = temp[0].length;
+		structure = temp;
 	}
 }
 
@@ -32,6 +70,7 @@ var level = function(width, height, p){
 	this.structure = new Array(height);
 	this.width = width;
 	this.height = height;
+
 	
 	var i;
 	
@@ -46,42 +85,45 @@ var level = function(width, height, p){
 
 		switch(num)
 		{
-		case 0:
-			return new piece([[1],[1],[1],[1]], '#8FEBE9', 0, left);
-			break;
-		case 1:
-			return new piece([[0,1,1],[1,1,0]], '#3ED936', 0, left);
-			break;
-		case 2:
-			return new piece([[1,1,0],[0,1,1]], '#F23E22', 0, left);
-			break;
-		case 3:
-			return new piece([[1,1,1],[0,1,0]], '#B21DCC', 0, left);
-			break;
-		case 4:
-			return new piece([[0,1],[0,1],[1,1]], '#194CC2', 0, left);
-			break;
-		case 5:
-			return new piece([[1,0],[1,0],[1,1]], '#F2B022', 0, left);
-			break;
-		case 6:
-			return new piece([[1,1],[1,1]], '#F2ED50', 0, left);
-			break;
+			case 0:
+				return new piece([[1],[1],[1],[1]], '#8FEBE9', 0, left);
+				break;
+			case 1:
+				return new piece([[0,1,1],[1,1,0]], '#3ED936', 0, left);
+				break;
+			case 2:
+				return new piece([[1,1,0],[0,1,1]], '#F23E22', 0, left);
+				break;
+			case 3:
+				return new piece([[1,1,1],[0,1,0]], '#B21DCC', 0, left);
+				break;
+			case 4:
+				return new piece([[0,1],[0,1],[1,1]], '#194CC2', 0, left);
+				break;
+			case 5:
+				return new piece([[1,0],[1,0],[1,1]], '#F2B022', 0, left);
+				break;
+			case 6:
+				return new piece([[1,1],[1,1]], '#F2ED50', 0, left);
+				break;
 		}
 	}
 	
-	this.active = p || this.createPiece();
+	this.active = p || this.createPiece();	
 
 	this.isObstructedLeft = function(){
-		if(this.active.left <= 0){
+		if(this.active.getLeft() <= 0){
 			return true;
 		}
-		for(i = 0; i < this.active.height; i++){
-			for(j = 0; j < this.active.width; j++){
-				if(this.active.structure[i][j] === 1){
-					var top = this.active.top;
+
+		var pieceStructure = this.active.getStructure();
+
+		for(i = 0; i < this.active.getHeight(); i++){
+			for(j = 0; j < this.active.getWidth(); j++){
+				if(pieceStructure[i][j] === 1){
+					var top = this.active.getTop();
 					if(top < 0) top = 0;
-					if(this.structure[top + i][this.active.left +j -1] != undefined){
+					if(this.structure[top + i][this.active.getLeft() +j -1] != undefined){
 						return true;
 					}
 					break;
@@ -92,15 +134,18 @@ var level = function(width, height, p){
 	}
 
 	this.isObstructedRight = function(){
-		if(this.active.left + this.active.width >= width){
+		if(this.active.getLeft() + this.active.getWidth() >= width){
 			return true;
 		}
-		for(i = 0; i < this.active.height; i++){
-			for(j = (this.active.width -1); j >= 0; j--){
-				if(this.active.structure[i][j] === 1){
-					var top = this.active.top;
+
+		var pieceStructure = this.active.getStructure();
+
+		for(i = 0; i < this.active.getHeight(); i++){
+			for(j = (this.active.getWidth() -1); j >= 0; j--){
+				if(pieceStructure[i][j] === 1){
+					var top = this.active.getTop();
 					if(top < 0) top = 0;
-					if(this.structure[top + i][this.active.left + j + 1] != undefined){
+					if(this.structure[top + i][this.active.getLeft() + j + 1] != undefined){
 						return true;
 					}
 					break;
@@ -111,18 +156,18 @@ var level = function(width, height, p){
 	}
 
 	this.isObstructedBottom = function(){
-		var nextRow;
-		for (i = (this.active.height - 1); i >= 0; i--){
-			nextRow = 1 + i + this.active.top;
+		var j, nextRow, pieceStructure = this.active.getStructure();
+		for (i = (this.active.getHeight() - 1); i >= 0; i--){
+			nextRow = 1 + i + this.active.getTop();
 			if(nextRow >= height){
 				return true;
 			}
 			if(nextRow < 0){
 				break;
 			}
-			for(j = 0; j <= (this.active.width -1); j++){
-				if(this.active.structure[i][j] === 1){
-					if(this.structure[nextRow][this.active.left + j] != undefined){
+			for(j = 0; j <= (this.active.getWidth() -1); j++){
+				if(pieceStructure[i][j] === 1){
+					if(this.structure[nextRow][this.active.getLeft() + j] != undefined){
 						return true;
 					}
 				}
@@ -132,12 +177,12 @@ var level = function(width, height, p){
 	}
 
 	this.placeActive = function(){
-		var j;
-		for(i = 0; i < this.active.height; ++i){
-			for (j = 0; j < this.active.width; ++j){
-				if(this.active.structure[i][j] == 1){
-					if((i + this.active.top) >= 0){
-						this.structure[i + this.active.top][j + this.active.left] = this.active.color;
+		var j, pieceStructure = this.active.getStructure();
+		for(i = 0; i < this.active.getHeight(); ++i){
+			for (j = 0; j < this.active.getWidth(); ++j){
+				if(pieceStructure[i][j] == 1){
+					if((i + this.active.getTop()) >= 0){
+						this.structure[i + this.active.getTop()][j + this.active.getLeft()] = this.active.getColor();
 					}	
 				}
 			}
@@ -175,7 +220,6 @@ var game = function(canvas, level, score, time){
 	this.score = score || 0;
 	this.time = time || 100;
 	this.status = "play";
-	this.fps = 0;
 	this.increment = 20;
 	this.canvas.width = this.level.width * this.increment;
 	this.canvas.height = this.level.height * this.increment;
@@ -189,25 +233,25 @@ var game = function(canvas, level, score, time){
 		switch (event.keyCode) {
 			case 37:
 				if(!that.level.isObstructedLeft()){
-					that.level.active.left -= 1;
+					that.level.active.moveLeft();
 				}				
 				break;
 			case 38:
 				that.level.active.rotate('cw');
 
-				if(that.level.active.left + that.level.active.width >= that.level.width){
-					that.level.active.left = (that.level.width - that.level.active.width);
+				if(that.level.active.getLeft() + that.level.active.getWidth() >= that.level.width){
+					that.level.active.setLeft(that.level.width - that.level.active.getWidth());
 				}
 
 				break;
 			case 39:
 				if(!that.level.isObstructedRight()){
-					that.level.active.left += 1;
+					that.level.active.moveRight();
 				}				
 				break;
 			case 40:
 				if(!that.level.isObstructedBottom()){
-					that.level.active.top += 1;
+					that.level.active.drop();
 				}				
 				break;
 			case 19:
@@ -231,20 +275,20 @@ var game = function(canvas, level, score, time){
 	}
 	
 	this.drawActive = function(){		
-		var i, j, x = that.increment;	
+		var i, j, x = that.increment, structure = that.level.active.getStructure();	
 		
-		context.fillStyle = that.level.active.color;	
-		for (i = 0; i < that.level.active.height; ++i){
-			for (j = 0; j < that.level.active.width; ++j){
-				if(that.level.active.structure[i][j] === 1){
+		context.fillStyle = that.level.active.getColor();	
+		for (i = 0; i < that.level.active.getHeight(); ++i){
+			for (j = 0; j < that.level.active.getWidth(); ++j){
+				if(structure[i][j] === 1){
 					context.fillRect(
-						(j + that.level.active.left) * x,
-						(i + that.level.active.top) * x,
+						(j + that.level.active.getLeft()) * x,
+						(i + that.level.active.getTop()) * x,
 						x, x
 					);
 					context.drawImage(img,
-						(j + that.level.active.left) * x,
-						(i + that.level.active.top) * x);
+						(j + that.level.active.getLeft()) * x,
+						(i + that.level.active.getTop()) * x);
 				}
 			}
 		}
@@ -271,16 +315,15 @@ var game = function(canvas, level, score, time){
 	}
 	
 	this.dropLoop = function(){
-		
 		if(that.level.isObstructedBottom()){
-			if(that.level.active.top < 0){
+			if(that.level.active.getTop() < 0){
 				that.status = "stop";
 				console.log("game over");
 			}
 			that.level.placeActive();
 			that.level.active = that.level.createPiece();
 		}else{
-			that.level.active.top++;	
+			that.level.active.drop();	
 		}
 
 		var fullRows = that.level.getFullRows();
